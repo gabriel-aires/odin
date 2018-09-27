482e925a52b2d02d92dfb701f3e5fb9013409ca5
gabriel-aires <gabriel-aires>
2018-09-27 23:43:35 +0000

    WIP: classes for generic gui input

--- desktop/main.tcl
+++ desktop/main.tcl
@@ -1,13 +1,12 @@
 package require Tk
 
 oo::class create Container {
-
 	variable Name Path
 
-	method setup {name path} {
+	method setup_container {name path} {
 		set Name $name
 		set Path $path
-		set frame [frame $Path]
+		set frame [::ttk::frame $Path]
 	}
 
 	method parent {} {
@@ -19,58 +18,104 @@ oo::class create Container {
 	}
 }
 
-oo::class create Section {
+oo::class create InputField {
+	method label_id {parent key} {
+		return "$parent.label_$key"
+	}
 	
-	superclass Container
-
-	constructor {args} {
-		my setup {*}$args
+	method input_id {parent key} {
+		return "$parent.input_$key"
 	}
 }
 
-oo::class create Form {
-
-	superclass Container
-	variable Children Submit
-
-	constructor {name path entries} {
-		my setup $name $path
-
-		foreach {input type} $entries {
-			switch $type {
-				text {
-					set widget [TxtInput new [my id] $input]
-					dict set Children $widget key $input
-					dict set Children $widget type $type
-				}
-				default {
-					error "Unsupported type: $type"
-				}
-			}
-		}
-
-		set Submit [button "[my id].submit" -text "OK" -command "[self] submit"]
+oo::class create Repository {
+	variable Repository
+	
+	method setup_repository {} {
+		array set Repository {}
+	}
+	
+	method repo_varname {name} {
+		return "[my varname Repository]($name)"
+	}
+	
+	method repo_value {name} {
+		return [array get Repository $name]
 	}
 }
 
 oo::class create TxtInput {
+	mixin -append InputField
 
-	variable Config Parent Key Value Submit
-
-	constructor {parent key} {
-		set Parent 	$parent
-		set Key 	$key
-		set Config(label)	[label 	"$Parent.label_$Key" 	-text $Key]
-		set Config(entry)	[entry 	"$Parent.entry_$Key" 	-textvariable [my varname Value] -background white -foreground black]
-		grid $Config(label) $Config(entry)
+	method setup_txt_input {parent key} {
+		array set Value {}
+		set label [my label_id $parent $key]
+		set entry [my input_id $parent $key]
+		::ttk::label $label -text $key
+		::ttk::entry $entry	-textvariable [my repo_varname $key] -background white -foreground black
+		grid $label $entry
 	}
 }
 
-set entries	{name text exec text pwd text options text}
+oo::class create Form {
+	mixin -append TxtInput
+	variable Entries
+
+	method setup_form {fields} {
+
+		foreach {key type} $fields {
+			switch -glob -- $type {
+				text:* {
+					set rule [lindex [split $type :] 1]
+					set parent [my id]
+					set child [my input_id $parent $key]
+					my setup_txt_input $parent $key
+					dict set Entries $child name $key
+					dict set Entries $child rule $rule
+				}
+				default {
+					puts "Unsupported type: $type"
+				}
+			}
+		}
+	}
+}
+
+oo::class create Section {
+	mixin -append Container
+	
+	constructor {args} {
+		my setup_container {*}$args
+	}
+}
+
+oo::class create AgentConfig {
+	mixin -append Container Form Repository
+	
+	constructor {args} {
+		set Name [lindex $args 0]
+		set Path [lindex $args 1]
+		set labels [lindex $args 2]
+		
+		my setup_repository
+		my setup_container $Name $Path
+		my setup_form $labels
+		
+		set submit [::ttk::button "[my id].submit" -text "Done" -command "[self] submit"]
+		grid $submit
+	}
+	
+	method submit {} {
+		set result [::ttk::label "[my id].result" -text "exec = [my repo_value exec]" ]
+		grid $result
+	}
+}
+
+set fields	{name text:required exec text:bool pwd text:required options text}
 set app		[Section new "app" ".app"]
 set left	[Section new "left" "[$app id].left"]
 set right	[Section new "right" "[$app id].right"]
-set form	[Form new "form" "[$left id].form" $entries ]
+set form	[AgentConfig new "form" "[$left id].form" $fields ]
 
 pack [$app id] -fill both
 pack [$left id] -side left -fill y
